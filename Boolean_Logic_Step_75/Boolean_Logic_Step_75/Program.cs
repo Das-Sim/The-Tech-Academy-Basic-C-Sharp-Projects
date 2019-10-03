517f66270ae4ca57c353954ac75caac08683ed85
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-03 12:50:52 -0500

    Upload Insuarnce Approval Boolean Example
    EXERCISE: BOOLEAN LOGIC
    
    Your job is to create a basic approval program for car insurance. It will ask the applicant some questions and return a “true” or “false” at the end as to whether or not the applicant qualified for car insurance.
    
    REQUIREMENTS
    
        Ask the following questions:
    
    What is your age?
    
    Have you ever had a DUI?
    
    How many speeding tickets do you have?
    
        Use the following qualification rules to determine if the applicant qualifies for car insurance:
    
    Applicants must be over 15 years old.
    
    Applicants must not have any DUI’s.
    
    Applicants must not have more than 3 speeding tickets.
    
        Print the result of the boolean expression created from the above business rules.

---
+++ Boolean_Logic_Step_75/Boolean_Logic_Step_75/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Approval_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your age?: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?: ");
+            Console.WriteLine(" 'True' or 'False' ");
+            bool drinksAndDrives = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?: ");
+            int caughtSpeeding = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool insuranceApproved = (age > 15 && drinksAndDrives == false && caughtSpeeding < 3);
+            Console.WriteLine(insuranceApproved);
+            Console.WriteLine("Have a nice day!");
+
+            Console.ReadLine();
+        }
+    }
+}
