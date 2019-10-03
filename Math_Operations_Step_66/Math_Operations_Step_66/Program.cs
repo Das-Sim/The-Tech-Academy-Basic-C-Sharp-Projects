fe7b4184bc803beb5f649f5200f3a1ac12fd525b
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-03 11:19:53 -0500

    Upload Example of Math Operations
    As part of The Tech Academy's C# and .NET Framework course, Step 66.
    
    CREATE A CONSOLE APPLICATION THAT DOES THE FOLLOWING:
    
    1. Takes an input from the user, multiplies it by 50, and prints the result to the console. (Note: make sure your code can take inputs larger than 10,000,000).
    
    2. Takes an input from the user, adds 25 to it, and prints the result to the console.
    
    3. Takes an input from the user, divides it by 12.5, and prints the result to the console.
    
    4. Takes an input from the user, checks if it is greater than 50, and prints the true/false result to the console.
    
    5. Takes an input from the user, divides it by 7, and prints the remainder to the console.

---
+++ Math_Operations_Step_66/Math_Operations_Step_66/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Operations_Step_66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter 1st number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num = num1 * 50;
+
+            Console.WriteLine(num);
+            Console.WriteLine("Press Enter to Continue.");
+            Console.ReadLine();
+            
+
+            Console.WriteLine("Enter 2nd number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num = num1 + 25;
+
+            Console.WriteLine(num);
+            Console.WriteLine("Press Enter to Continue.");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter 3rd number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            double num2 = Convert.ToDouble(num);
+            num2 = num1 / 12.5;
+
+            Console.WriteLine(num2);
+            Console.WriteLine("Press Enter to Continue.");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter 4th number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            int num3 = 50;
+            bool isGreater = num1 > num3;
+            
+            Console.WriteLine(isGreater);
+            Console.WriteLine("Press Enter to Continue.");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter 5th number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num = num1 % 7;
+
+            Console.WriteLine(num);
+            Console.WriteLine("Press Enter to Continue.");
+            Console.ReadLine();
+            
+        }
+    }
+}
