55f5850d77f1aacf17d0381af290f36a6654724c
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-03 11:52:26 -0500

    Upload Value Comparison Example
    As part of The Tech Academy's C# and .NET Framework course, Step 67.
    
    EXERCISE: MATH AND COMPARISON OPERATORS
    
    Your job is to create an anonymous income comparison program.
    
    REQUIREMENTS
    
        The program must start by printing “Anonymous Income Comparison Program” to the screen.
    
        It must then print “Person 1” to the screen and get the following details:
    
    Hourly Rate
    
    Hours worked per week
    
        It must then print “Person 2” to the screen and then get the following details:
    
    Hourly rate
    
    Hours worked per week
    
        It must then print to the screen “Weekly salary of Person 1:” and write the exact salary below it.
    
        It must then print to the screen “Weekly salary of Person 2:” and write the exact salary below it.
    
        It must then print to the screen “Does Person 1 make more money than Person 2?” and write the true or false value of this statement below it.

---
+++ Salary_Compare_Example/Salary_Compare_Example/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary_Compare_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            //Person 1
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate? ");
+            decimal hRate = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hours worked per week? ");
+            decimal hWork = decimal.Parse(Console.ReadLine());         
+
+            decimal salary = hRate * hWork;
+
+            //Person 2
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate? ");
+            decimal hRate2 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hours worked per week? ");
+            decimal hWork2 = decimal.Parse(Console.ReadLine());
+
+            decimal salary2 = hRate2 * hWork2;
+
+            bool compare = true;
+            compare = (salary > salary2);
+
+            if (compare)
+            {
+                compare = true;
+            }
+
+            if (!compare)
+            {
+                compare = false;
+            }
+
+            Console.WriteLine("Weekly salary of Person 1: " + salary);
+            Console.WriteLine("Weekly salary of Person 2: " + salary2);
+            Console.WriteLine("Does Person 1 make more money than Person 2?: " + compare);
+            Console.WriteLine("Have a good day!");
+            Console.ReadLine();
+
+        }
+    }
+}
