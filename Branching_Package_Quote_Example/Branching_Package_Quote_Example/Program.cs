ac361089955355ff5e81cb9f0e1bd370316712b3
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-03 17:03:14 -0500

    Upload Shipping Quote Example Project
    As part of The Tech Academy's C# and .NET Framework course, Step 92.
    
    EXERCISE: BRANCHING
    
    Your job is to create a console-based application for getting a shipping quote for a package.
    
    REQUIREMENTS
    
        The first line of the program must be: “Welcome to Package Express. Please follow the instructions below.”
    
        The user must then be prompted for the package weight.
    
        If the weight is greater than 50, display the error message, “Package too heavy to be shipped via Package Express. Have a good day.” At this point the program would end.
    
        The user must then be prompted for the package width.
    
        Then the package height.
    
        Then the package length.
    
        If the sum of the dimensions is greater than 50, display the error message, “Package too big to be shipped via Package Express.” At this point the program would end.
    
        The sum total of dimensions are then multiplied by the weight and divided by 100.
    
        The result of that calculation is the quote.
    
        Display the quote to the user as a dollar amount.

---
+++ Branching_Package_Quote_Example/Branching_Package_Quote_Example/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping_Quote_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Press Enter to Continue.");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter package weight: ");
+            string pWeight = Console.ReadLine();
+            int weight = Convert.ToInt32(pWeight);
+
+
+            //if for when Package is too heavy
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter the package width: ");
+            string pWidth = Console.ReadLine();
+            int width = Convert.ToInt32(pWidth);
+
+            Console.WriteLine("Please enter the package height: ");
+            string pHeight = Console.ReadLine();
+            int height = Convert.ToInt32(pHeight);
+
+            Console.WriteLine("Please enter the package length: ");
+            string pLength = Console.ReadLine();
+            int length = Convert.ToInt32(pLength);
+
+
+            decimal pSize = width + height + length;
+            if (pSize > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            decimal quote = pSize * weight / 100.00m;
+            Console.WriteLine("Your estimated total for shipping this package is : $" + quote + "\r\nThank you.");
+            Console.ReadLine();
+
+        }
+    }
+}
