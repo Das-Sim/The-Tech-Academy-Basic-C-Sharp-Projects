0726e8f3e20e29de5474019a7a9a37d830d112c8
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-07 14:59:48 -0500

    Add String Concatination Example
    As part of The Tech Academy's C# and .NET Framework Course, Step 97.
    
    EXERCISE:
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
        Concatenate three strings.
    
        Convert a string to uppercase.
    
        Create a Stringbuilder and build a paragraph of text, one sentence at a time.

---
+++ Strings_Example/Strings_Example/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to my example of String Concatination in C#! \n Press ENTER to continue to the program.");
+            Console.ReadLine();
+
+            string name = "Dan ";
+            string mname = "The ";
+            string lname = "Man";
+            Console.WriteLine(name);
+            Console.WriteLine(mname);
+            Console.WriteLine(lname);
+
+            Console.WriteLine();
+            Console.WriteLine("Here we have made 3 strings. \n Please press ENTER.");
+            Console.ReadLine();
+
+            string answer = name + mname + lname;
+
+            answer = answer.ToUpper();
+
+            Console.WriteLine(answer);
+
+            Console.WriteLine();
+            Console.WriteLine("Here we take 3 seperate strings, concatinate them, change the strings to uppercase, and form a person's whole name!");
+            Console.WriteLine("Please press ENTER.");
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Dan! ");
+            sb.Append("My middle name is The! ");
+            sb.Append("My last name is Man!");
+            Console.WriteLine();
+
+            Console.WriteLine("Using StringBuilder we can change strings without performance loss when dealing with large projects");
+
+            Console.WriteLine();
+            Console.WriteLine(sb);
+            Console.WriteLine();
+            Console.WriteLine("Please press ENTER.");
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for using my program, Have a good day!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
