61f87e197b6dce6a38dd57d73ae151e52d350505
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-07 16:58:54 -0500

    Add Iteration Examples
    Examples from TTA.

---
+++ Iteration/Iteration/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //enter test scores here
+            //int[] testScores = { 98, 99, 95, 70, 82, 56, 90, 91, 89 };
+
+            //for (int i = 0; i < testScores.Length; i++)
+            //{
+            //  if (testScores[i] > 85)
+            //  {
+            //      Console.WriteLine("Passing test Scores: " + testScores[i]);
+            //  }
+            // }
+            // Console.ReadLine();
+
+            //string example
+            //string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+            //for (int j = 0; j < names.Length; j++)
+            // {
+            //   if (names[j] == "Jesse")
+            //   {
+            //       Console.WriteLine(names[j]);
+            //   }
+            // }
+            // Console.ReadLine();
+
+            //list iteration example with integers
+            //List<int> testScores = new List<int>();
+            //testScores.Add(98);
+            //testScores.Add(97);
+            //testScores.Add(92);
+            //testScores.Add(98);
+            //testScores.Add(72);
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        Console.WriteLine("Passing Test Score: " + score);
+
+            //    }
+
+            //}
+            //Console.ReadLine();
+
+            //list iteration example with strings
+            //List<string> names = new List<string>() { "Jessie", "Erik", "Adam", "Daniel" };
+
+            //foreach (string name in names)
+            //{
+
+            //        Console.WriteLine(name);         
+            //}
+            //Console.ReadLine();
+
+            //iterate how many passing scores with number of passes
+            //List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+            //List<int> passingScores = new List<int>();
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        passingScores.Add(score);
+            //    }
+            //}
+            //Console.WriteLine(passingScores.Count);
+            //Console.ReadLine();
+
+        }
+    }
+}
