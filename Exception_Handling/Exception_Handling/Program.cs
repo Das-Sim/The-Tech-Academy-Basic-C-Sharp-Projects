756d29bbe3b0e50de85f0944fa6d7f3b7239fd05
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-08 15:42:46 -0500

    Upload Exception Handling Mini Game
    As part of The Tech Academy's C# and .NET Framework Course, Step 106.
    
    EXERCISE:
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
    1. Create a list of integers. Ask the user for a number to divide each number in the list by. Write a loop that takes each integer in the list, divides it by the number the user entered, and displays the result to the screen.
    
    2. Run that code, entering in non-zero numbers as the user. Look at the displayed results.
    
    3. Run that code, entering in zero as the number to divide by. Note any error messages you get.
    
    4. Run that code, entering in a string as the number to divide by. Note any error messages you get.
    
    5. Now put the loop in a try/catch block. Below and outside of the try/catch block, make the program display a message to the display to let you know the program has emerged from the try/catch block and continued on with program execution. In the catch block, display the error message to the screen. Then try various combinations of user input: valid numbers, zero and a string. Ensure the proper error messages display on the screen, and that the code after the try/catch block gets executed.

---
+++ Exception_Handling/Exception_Handling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("\nHELLOOO DOWN THERE! This my exception handling ladder.\n Please, Press ENTER to begin your ascent.");
+            Console.ReadLine();
+            List<int> intList = new List<int>() { 1090, 2000, 5000, 2300, 5220, 1000, 5400 };          
+            foreach (int num in intList)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.Write("\nThere are different ladders to choose from! \n Curious? You think it will take too long to get to the top?\n Please, be my guest, give me any whole number and I will divide your ladders by it. ");
+            Console.WriteLine();
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nSo " + num1 + " huh..");
+            foreach (int num in intList)
+            {
+                Console.WriteLine("Ladder: " + num + " divided by " + num1 + " equals " + (num / num1) + " moves.");
+            }
+        }
+
+
+        catch (FormatException ex)
+        {
+            Console.WriteLine("\nPlease use a whole number.");
+        }
+
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("\nPlease no dividing by zero..");
+        }
+
+        Console.WriteLine("\nNow falling off the exception handling ladder!! Enjoy the rest of the game!");
+        Console.WriteLine("Press ENTER to continue.");
+        Console.ReadLine();
+
+        Console.WriteLine("Thank you for viewing my program. Have a good day.");
+        Console.ReadLine();
+    }
+}
