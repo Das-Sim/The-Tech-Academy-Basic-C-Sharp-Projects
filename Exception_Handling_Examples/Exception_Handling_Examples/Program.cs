56056ec63441bb6125d2c50a86bc884833023126
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-08 15:16:51 -0500

    Upload Examples of Exception Handling
    As part of TTA.

---
+++ Exception_Handling_Examples/Exception_Handling_Examples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        try
+        {
+            Console.WriteLine("Pick a number. ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number. ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Diving the two...");
+            int num3 = num1 / num2;
+            Console.WriteLine(num1 + " divided by " + num2 + " equals " + num3);
+            Console.ReadLine();
+        }
+
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please provide a whole number.");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please no dividing by zero here.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }   
+        
+        }
+    }
