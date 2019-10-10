7d3b99e0f48cf57d870b10b2fdb1afe68a0b69f5
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-10 12:08:03 -0500

    Upload Class and Methods Practice
    As Part of The Tech Academy's C# and .NET Framework Course, Step 115.
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
    1. Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. Have the method do a math operation and return an integer result.
    
    2. In the Main() method of the console app, instantiate the class.
    
    3. Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
    
    4. Call the method in the class, passing in the one or two numbers entered.
    
    5. Try various combinations of numbers on the code, including having no second number.

---
+++ Class_Methods_Practice_4/Class_Methods_Practice_4/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods_Practice_4
+{
+    //Create a class.  
+    class Class1
+    {
+        public int num(int num1, int num2 = 5) //Create a method that takes two integers as parameters. Make one of them optional.
+        {
+            Console.WriteLine(num1 + " plus " + num2 + " = "); //Have the method do a math operation and return an integer result.
+            return (num1 + num2);
+        }
+    }
+}
---
+++ Class_Methods_Practice_4/Class_Methods_Practice_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods_Practice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method of the console app, instantiate the class.
+            Class1 prac = new Class1();
+
+            //Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
+            Console.WriteLine("Hello and Welcome to my Program!\n Please enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());           
+            Console.WriteLine("You can enter a second number (default is 5). Press ENTER if you'd rather not enter the 2nd number: ");
+            string txt = Console.ReadLine();
+            if (Int32.TryParse(txt, out int txt2))
+            {
+                Console.WriteLine(prac.num(num1, txt2)); ////Call the method in the class, passing in the one or two numbers entered.
+            }
+            else
+            {
+                Console.WriteLine(prac.num(num1)); //Try various combinations of numbers on the code, including having no second number.
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for viewing my program. Have a good day.");
+            Console.ReadLine();
+        }
+    }
+}
