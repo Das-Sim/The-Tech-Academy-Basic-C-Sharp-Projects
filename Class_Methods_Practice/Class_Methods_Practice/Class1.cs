5bff09a319e3c5dc4e801387f7d2eb7b116c5934
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-10 10:41:51 -0500

    Upload Item Shop
    As part of The Tech Academy's C# and .NET Framework Course, Step 111.
    
    EXERCISE:
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
    1. Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer. The methods should do some math operation on the received parameter. Put this class in a separate .cs file in the application.
    
    2. In the Main() program, ask the user what number they want to do the math operations on.
    
    3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen.

---
+++ Class_Methods_Practice/Class_Methods_Practice/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods_Practice
+{
+    class Class1
+    {
+        //1. Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer. 
+        //The methods should do some math operation on the received parameter. Put this class in a separate .cs file in the application.
+
+        //2. In the Main() program, ask the user what number they want to do the math operations on.
+
+        //3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
+
+        public int Sell(int input1)
+        {
+            int product1 = input1;
+            int profit1 = 2 * (product1);
+            return profit1;
+        }
+
+        public int Buy(int input2)
+        {
+            int cost1 = input2 - 25;
+            return cost1;
+        }
+        public int Trade(int input3)
+        {
+            int trade1 = input3;
+            return trade1;
+        }
+    }
+}
---
+++ Class_Methods_Practice/Class_Methods_Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 math = new Class1();
+            Console.WriteLine("Hello traveller, welcome to my shop.\n Here you can BUY, SELL, or TRADE your hard earned loot.\n Let's get started. Press ENTER to continue.");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter how many wooden logs you would like to sell.\n");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int result1 = math.Sell(input1);
+            Console.WriteLine(result1 + " gold earned.\n");
+
+            Console.WriteLine("Enter how many potions you would like to buy.\n");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            int result2 = math.Buy(input2);
+            Console.WriteLine(result2 + " gold owed.\n");
+
+            Console.WriteLine("Enter how many apples you like to trade.\n");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+            int result3 = math.Trade(input3);
+            Console.WriteLine(result3 + " oranges recieved. Trade complete.\n Thanks for stopping by the shop.\n Press ENTER to continue.");
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for viewing my program. Have a good day.");
+            Console.ReadLine();
+        }
+    }
+}
