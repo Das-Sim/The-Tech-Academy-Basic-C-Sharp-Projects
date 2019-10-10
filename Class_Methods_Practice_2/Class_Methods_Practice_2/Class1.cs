192952e913a97b5996b4feddb95654f64b233170
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-10 11:21:18 -0500

    Upload Methods Practice 2
    As part of The Tech Academy's C# and .NET Framework Course, Step 113.
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
    1. Create a class. In that class, create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen.
    
    2. In the Main() method of the console app, instantiate the class.
    
    3. Call the method in the class, passing in two numbers.
    
    4. Call the method in the class, specifying the parameters by name.

---
+++ Class_Methods_Practice_2/Class_Methods_Practice_2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods_Practice_2
+{
+    //Create a class. In that class,
+    class Class1
+    {
+        public void num(int num1, int num2) //create a void method that takes two integers as parameters. 
+        {  
+            int result = num1 * 42; //Have the method do a math operation on the first integer and display the second integer to the screen.
+            Console.WriteLine(result);
+            Console.WriteLine(num2 + " was the second number.");//Call the method in the class, specifying the parameters by name.
+        }
+    }
+}
---
+++ Class_Methods_Practice_2/Class_Methods_Practice_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods_Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 prac = new Class1(); //In the Main() method of the console app, instantiate the class.
+            prac.num(4, 2); //Call the method in the class, passing in two numbers.         
+            Console.ReadLine();
+        }
+    }
+}
