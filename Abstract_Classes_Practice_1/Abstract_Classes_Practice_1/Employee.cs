40d2d074a89877879ab5f4bcf72a32e30ab18503
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-10 17:13:46 -0500

    Updated with Overloaded "== & !=" Operators
    As Part of The Tech Academy's C# and .NET Framework Course, Step 129.
    
    EXERCISE:
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
        Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.

--- Abstract_Classes_Practice_1/Abstract_Classes_Practice_1/Employee.cs
+++ Abstract_Classes_Practice_1/Abstract_Classes_Practice_1/Employee.cs
@@ -7,16 +7,25 @@ using System.Threading.Tasks;
 namespace Abstract_Classes_Practice_1
 {
     //3. Create another class called Employee. 
-    public class Employee : Person, IQuittable //- Have it inherit from the Person class. //2a. Have your Employee class from the previous drill inherit IQuittable. 
+    public class Employee : Person//, IQuittable //- Have it inherit from the Person class. //2a. Have your Employee class from the previous drill inherit IQuittable. 
     {
+        public int ID { get; set; }
         public override void SayName()
         {
-            Console.WriteLine();
-            Console.WriteLine(firstName + " " + lastName);          
-        }
-        public void Quit() //- Implement the Quit() method in any way you choose.
-        {
+            Console.WriteLine(firstName + " " + lastName);
 
         }
+        public static bool operator ==(Employee ID1, Employee ID2) //Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property. 
+        {
+            return ID1.ID.Equals(ID2.ID);
+        }
+
+        public static bool operator !=(Employee ID1, Employee ID2) //Overload the "!=" operator so it checks if two Employee objects are equal by comparing their Id property. 
+        {
+            return !ID1.ID.Equals(ID2.ID);
+        }
+
     }
+
+    //public void Quit() //- Implement the Quit() method in any way you choose.
 }
--- Abstract_Classes_Practice_1/Abstract_Classes_Practice_1/Program.cs
+++ Abstract_Classes_Practice_1/Abstract_Classes_Practice_1/Program.cs
@@ -12,12 +12,24 @@ namespace Abstract_Classes_Practice_1
         {
             //5. Inside the Main() method,    
             Employee temp = new Employee(); //- instantiate an Employee object with:
+            Employee temp1 = new Employee();
             temp.firstName = "Sample"; //- firstName "Sample"
             temp.lastName = "Student"; //- and lastName "Student".
+            temp.ID = 1;
+            temp1.ID = 2;
             temp.SayName(); //- Call the SayName() method on the object.
+            if(temp.ID == temp1.ID)
+            {
+                Console.WriteLine("ID's are matching."); //Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property. 
+            }
+            else
+            {
+                Console.WriteLine("No matching ID's were found."); //Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+            }
             Console.ReadLine();
-            IQuittable leave = new Employee(); //3a. Use polymorphism to create an object of type IQuittable. 
-            leave.Quit(); //- Call the Quit() method on it.
+            //IQuittable leave = new Employee(); //3a. Use polymorphism to create an object of type IQuittable. 
+            //leave.Quit(); //- Call the Quit() method on it.          
+            
         }
     }
 }
