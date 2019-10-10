06031d341f7975382d31c5a1a2d3b8e4cfec8514
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-10 11:50:07 -0500

    Upload Methods Practice
    As Part of The Tech Academy's C# and .NET Framework Course, Step 114
    
    EXERCISE:
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
    1. Create a class. In that class, create a method that will take in an integer, do a math operation to it and then return the answer as an integer.
    
    2. In the Main() method of the console app, instantiate the class and call the one method, passing in an integer, such as 12. Then display the result to the screen.
    
    3. Add a second method to the class, with the same name, that will take in a decimal, do a different math operation to it and then return the answer as an integer.
    
    4. In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
    
    5. Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it and then return the answer as an integer.
    
    6. In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.

---
+++ Class_Method_Practice_3/Class_Method_Practice_3/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Practice_3
+{
+    //1. Create a class.  
+    
+    class Class1
+    {
+        public int num(int num1) //In that class, create a method that will take in an integer,
+        {
+            int result = (num1 + 5); ////do a math operation to it and then return the answer as an integer.
+            return num1;
+        }
+
+        public decimal num(decimal num1) //3. Add a second method to the class, with the same name, that will take in a decimal 
+        {
+            decimal result1 = (num1 * 5); //do a different math operation to it and then return the answer as an integer.
+            return num1;
+        }
+
+        public int num(string str1) //5. Add a third method to the class, with the same name, that will take in a string
+        {
+            int prac;
+            int.TryParse(str1, out prac); //convert it to an integer if possible, do a different math operation to it and then return the answer as an integer.
+            return prac / 5;
+        }
+    }
+}
---
+++ Class_Method_Practice_3/Class_Method_Practice_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Practice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.In the Main() method of the console app            
+            Class1 prac = new Class1(); //instantiate the class          
+            Console.WriteLine(prac.num(12)); //call the one method, passing in an integer, such as 12. Then display the result to the screen.
+            Console.WriteLine(prac.num(100.00m)); //call the second method, passing in a decimal. Display the result to the screen.
+            Console.WriteLine(prac.num(112)); //call the third method, passing in a string that equates to an integer. Display the result to the screen.
+            Console.ReadLine();
+
+
+        }
+    }
+}
