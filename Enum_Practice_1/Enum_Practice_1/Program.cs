01ba218182ab0f3f2772adc717acbd3f90ccaf8e
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-11 14:11:25 -0500

    Upload Example of Enum Usage
    As Part of The Tech Academy's C# and .NET Framework Course, Step 134.
    
    EXERCISE:
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
    1. Create an enum for the days of the week.
    
    2. Prompt the user to enter the current day of the week.
    
    3. Assign the value to a variable of that enum data type you just created.
    
    4. Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week." to the console if an error occurs.
    
    Note - In the last video, we explained the underlying concepts behinds enums. Nobody knows everything about a programming language and its features, so it's our job as programmers to do research on how to work with various data types, classes and libraries. For this drill, you'll need to do some additional research on how to parse enums.

---
+++ Enum_Practice_1/Enum_Practice_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User! That was quite an impact, but the ship is still in tact! Can you tell me what day it is?"); //2a. Prompt the user to enter the current day of the week.
+            string startingDay = Console.ReadLine(); //3a. Assign the value to a variable of that enum data type you just created.
+
+            try
+            {
+                DaysOfTheWeek day1 = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), startingDay);
+                Console.WriteLine(day1 + "?\n We'd better get moving..");
+                Console.ReadLine();
+                Console.WriteLine("Thank you for viewing my program. Have a good day.");
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week. Because of your actions, today's day is: DOOMS DAY! AND THE PROGRAM WILL NOW END!");             
+            }
+            Console.Read(); //4b. Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week." to the console if an error occurs.
+        }
+
+        
+
+        public enum DaysOfTheWeek //1a. Create an enum for the days of the week.
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        } 
+    }
+}
