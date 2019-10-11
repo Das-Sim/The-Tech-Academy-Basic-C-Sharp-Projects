9949ae4662672a4993b11859795795f13742b2bf
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-11 14:22:51 -0500

    Uploade Example of Using Structs
    As Part of The Tech Academy's C# and .NET Framework Course, Step 137.
    EXERCISE:
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
    1. Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
    
    2. In the Main() method, create an object of data type Number and assign an amount to it.
    
    3. Print this amount to the console.

---
+++ Struct_Practice_1/Struct_Practice_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Practice_1
+{
+    class Program
+    {
+        public struct Number //1a. Create a struct called Number.
+        {
+            public decimal Amount; //- Give it the property "Amount" and have it be of data type decimal.
+        }
+        static void Main(string[] args)
+        {                    
+            Number num1 = new Number(); //2a. In the Main() method, create an object of data type Number and assign an amount to it.
+            num1.Amount = 19.19m;
+            Console.WriteLine("Amount: " + num1.Amount); //3a.Print this amount to the console.
+            Console.WriteLine("Thank you for viewing my program. Have a good day.");
+            Console.Read();
+        }
+    }
+    
+}
